1cd34c5b60df89c4f9e0ed6dba594c12db248f76
jeeva-eng <jeevanandhan127@gmail.com>
2026-02-10 21:49:03 +0530

    Fix chat using session storage

--- app.py
+++ app.py
@@ -3,16 +3,25 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
+from fastapi.middleware.sessions import SessionMiddleware
+
 from src.search import RAGSearch
 
 import os
-import os
+
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
 # Create app
 app = FastAPI()
+
+
+# Session middleware (IMPORTANT)
+app.add_middleware(
+    SessionMiddleware,
+    secret_key="rag-secret-key-123"
+)
 
 
 # Templates folder
@@ -25,7 +34,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Load RAG once (important for performance)
 rag = None
-chat_history = []
+
 
 def get_rag():
     global rag
@@ -34,13 +43,19 @@ def get_rag():
     return rag
 
 
-# Home page
+# Home page (Reset chat here)
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
 
+    # Clear old chat on new visit
+    request.session["chat"] = []
+
     return templates.TemplateResponse(
         "index.html",
-        {"request": request}
+        {
+            "request": request,
+            "chat": []
+        }
     )
 
 
@@ -48,20 +63,24 @@ def home(request: Request):
 @app.post("/search", response_class=HTMLResponse)
 def search(request: Request, query: str = Form(...)):
 
-    global chat_history
+    # Get chat from session
+    chat = request.session.get("chat", [])
 
     rag_system = get_rag()
     answer = rag_system.search_and_summarize(query, top_k=5)
 
-    chat_history.append({
+    chat.append({
         "user": query,
         "bot": answer
     })
+
+    # Save back to session
+    request.session["chat"] = chat
 
     return templates.TemplateResponse(
         "index.html",
         {
             "request": request,
-            "chat": chat_history
+            "chat": chat
         }
     )
